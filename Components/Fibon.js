5f2b51b37bf9553b639700fb5c8e8eda713648eb
praveen <pravinkabi6@gmail.com>
2023-03-02 20:16:19 +0530

    f-com

---
+++ Components/Fibon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Number from '../Json/Number.json'
+import Prime from '../Json/Fibo.json'
+export default function Fibon() {
+  return (
+    <div>
+        {
+       Number.numbers
+        }
+        <br/>
+    </div>
+  )
+}
